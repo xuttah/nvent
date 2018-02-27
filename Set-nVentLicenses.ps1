d6b8ebe59b48ce317f9bc9ab8e0f8f03616bb7cd
Kevin Clark <kevin.clark@clearpointe.com>
2018-02-27 17:04:50 -0600

    Added license options

--- Set-nVentLicenses.ps1
+++ Set-nVentLicenses.ps1
@@ -23,12 +23,24 @@
 Import-Module MSOnline
 Connect-MsolService
 
+#Build nVent custom SKUs
+###### Connect to Office 365 GOES HERE ######
+# Define E3 custom SKU with only critical services
+$nVentE3Options = New-MsolLicenseOptions -AccountSkuId nventco:ENTERPRISEPACK -DisabledPlans BPOS_S_TODO_2,FORMS_PLAN_E3,STREAM_O365_E3,Deskless,FLOW_O365_P2,POWERAPPS_O365_P2,TEAMS1,PROJECTWORKMANAGEMENT,SWAY,INTUNE_O365,YAMMER_ENTERPRISE,RMS_S_ENTERPRISE
+$nVentF1Options = New-MsolLicenseOptions -AccountSkuId nventco:ENTERPRISEPACK -DisabledPlans #list sub-SKUs
+# Assign E3 SKUs
 $users = import-csv .\nVent_Users.csv
 foreach ($user in $users)
 {
     $upn=$user.UPN
     $usagelocation=$user.country 
-    $SKU=$user.SKU
-    Set-MsolUser -UserPrincipalName $upn -UsageLocation $usagelocation
-    Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $SKU
+    If ($user.SKU = "E3"){
+        Set-MsolUser -UserPrincipalName $upn -UsageLocation $usagelocation
+        Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $SKU -LicenseOptions $nVentE3Options
+    }
+    If ($user.SKU = "F1"){
+        Set-MsolUser -UserPrincipalName $upn -UsageLocation $usagelocation
+        Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $SKU -LicenseOptions $nVentF1Options
+    }
+
 } 
